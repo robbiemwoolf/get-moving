23c9fc3e63e7e9d4bdf5599942788988ffd8a874
Robbie Woolf <robbiemwoolf@gmail.com>
2023-04-13 01:03:22 -0700

    habits data moved; unique child key added

--- src/habits/Habits.jsx
+++ src/habits/Habits.jsx
@@ -1,48 +1,15 @@
 import { useState } from 'react'
 import './Habits.css'
-import read from '../assets/read.svg'
-import water from '../assets/water.svg'
-import steps from '../assets/steps.svg'
-import strength from '../assets/strength.svg'
+import habits from '../data/habits'
 
 export default function Habits() {
-    const habits = [
-        {
-            name: 'read',
-            details: 'Read for thirty minutes.',
-            icon: read,
-            alt: 'book',
-            isComplete: false  
-        },
-        {
-            name: 'water',
-            details: 'Drink one gallon of water.',
-            icon: water,
-            alt: 'water droplet',
-            isComplete: false  
-        },
-        {
-            name: 'steps',
-            details: 'Go outside. Take 15,000 steps.',
-            icon: steps,
-            alt: 'footsteps',
-            isComplete: false  
-        },
-        {
-            name: 'strength',
-            details: 'Complete a strength routine.',
-            icon: strength,
-            alt: 'dumbbell',
-            isComplete: false  
-        }
-    ]
 
     const habitBtns = habits.map((habit) => {
         const [isComplete, setIsComplete] = useState(false)
         const handleClick = () => {setIsComplete(!isComplete)}
         const toggle = isComplete ? 'complete' : null
         return (
-            <button className={`daily ${toggle}`} onClick={handleClick}><img src={habit.icon} alt='habit.alt' /></button> 
+            <button className={`daily ${toggle}`} onClick={handleClick} key={habit.name}><img src={habit.icon} alt={habit.alt} key={habit.name} /></button> 
         )
     })
 
