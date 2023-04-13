6992c5adfcb4a2a6f7c449799a6c9f240e5e734d
Robbie Woolf <robbiemwoolf@gmail.com>
2023-04-13 02:19:58 -0700

    Table function to be used for each strength day

---
+++ src/tools/Table.jsx
@@ -0,0 +1,22 @@
+import '../pages/day.css'
+import plans from '../data/plans'
+
+export default function Table(index) {
+    const day = plans[index].day
+    const list = plans[index].exercises.map((exercise) => {
+        return (
+            <div className='row' key={exercise.name}>
+                <div className='cell exercise'>{exercise.name}</div>
+                <div className='cell sets'>{exercise.sets}</div>
+                <div className='cell reps'>{exercise.reps}</div>
+                <div className='cell weight'>{exercise.weight}</div>
+            </div>
+        )
+    })
+    return (
+        <>
+            <h1 id={day}>{day} day</h1>
+            {list}
+        </>
+    )
+}
