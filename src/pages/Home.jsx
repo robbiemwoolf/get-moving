5682ff172ba8906a14e46f5d41635779c1cfd9a5
Robbie Woolf <robbiemwoolf@gmail.com>
2023-04-15 10:49:18 -0700

    removed map

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,35 @@
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Home.css'
 import Habits from '../habits/Habits'
 
-export default function Home() {
-    const days = ['Push', 'Pull', 'Leg', 'Upper', 'Lower']
-    const [isComplete, setIsComplete] = useState(false)
-    const toggle = isComplete ? 'complete' : null
-    const cards = days.map((day) => {
-        return (
-            <Link to={day} key={day}>
-                <button className={`dayBtn ${toggle}`}>{day} Day</button>
-            </Link>
-        )
-    })
+
+export default function Home({pushDone, pullDone, legDone, upperDone, lowerDone}) {
+
+    const pushToggle = pushDone ? 'complete' : null
+    const pullToggle = pullDone ? 'complete' : null
+    const legToggle = legDone ? 'complete' : null
+    const upperToggle = upperDone ? 'complete' : null
+    const lowerToggle = lowerDone ? 'complete' : null
 
     return (
         <>
             <Habits />
             <div className='col'>
-                {cards}
+                <Link to='push'>
+                    <button className={`dayBtn ${pushToggle}`}>push</button>
+                </Link>
+                <Link to='pull'>
+                    <button className={`dayBtn ${pullToggle}`}>pull</button>
+                </Link>
+                <Link to='leg'>
+                    <button className={`dayBtn ${legToggle}`}>leg</button>
+                </Link>
+                <Link to='upper'>
+                    <button className={`dayBtn ${upperToggle}`}>upper</button>
+                </Link>
+                <Link to='lower'>
+                    <button className={`dayBtn ${lowerToggle}`}>lower</button>
+                </Link>
             </div>
         </>
     )
