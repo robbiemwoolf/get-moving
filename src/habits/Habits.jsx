638fd127fe5e1e337bf9c3cd70185748e565649b
Robbie Woolf <robbiemwoolf@gmail.com>
2023-04-15 10:49:47 -0700

    removed map; still not tracking state across pages

--- src/habits/Habits.jsx
+++ src/habits/Habits.jsx
@@ -3,19 +3,22 @@ import './Habits.css'
 import habits from '../data/habits'
 
 export default function Habits() {
+    const [zeroDone, setZeroDone] = useState(false)
+    const [oneDone, setOneDone] = useState(false)
+    const [twoDone, setTwoDone] = useState(false)
+    const [threeDone, setThreeDone] = useState(false)
 
-    const habitBtns = habits.map((habit) => {
-        const [isComplete, setIsComplete] = useState(false)
-        const handleClick = () => {setIsComplete(!isComplete)}
-        const toggle = isComplete ? 'complete' : null
-        return (
-            <button className={`daily ${toggle}`} onClick={handleClick} key={habit.name}><img src={habit.icon} alt={habit.alt} key={habit.name} /></button> 
-        )
-    })
-
+    const zeroToggle = zeroDone ? 'complete' : null
+    const oneToggle = oneDone ? 'complete' : null
+    const twoToggle = twoDone ? 'complete' : null
+    const threeToggle = threeDone ? 'complete' : null
+    
     return (
         <div className='habits'> 
-            {habitBtns}
+            <button className={`daily ${zeroToggle}`} onClick={() => {setZeroDone(!zeroDone)}} key={habits[0].name}><img src={habits[0].icon} alt={habits[0].alt} /></button> 
+            <button className={`daily ${oneToggle}`} onClick={() => {setOneDone(!oneDone)}} key={habits[1].name}><img src={habits[1].icon} alt={habits[1].alt} /></button> 
+            <button className={`daily ${twoToggle}`} onClick={() => {setTwoDone(!twoDone)}} key={habits[2].name}><img src={habits[2].icon} alt={habits[2].alt} /></button> 
+            <button className={`daily ${threeToggle}`} onClick={() => {setThreeDone(!threeDone)}} key={habits[3].name}><img src={habits[3].icon} alt={habits[3].alt} /></button> 
         </div>
     )
 }
