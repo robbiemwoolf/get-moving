b8f655e39e6024b1f93e21e349478a913b75aae6
Robbie Woolf <robbiemwoolf@gmail.com>
2023-04-15 10:45:19 -0700

    moved workout states to App level

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
@@ -8,14 +9,20 @@ import Upper from './pages/Upper'
 import Lower from './pages/Lower'
 
 function App() {
+    const [pushDone, setPushDone] = useState(false)
+    const [pullDone, setPullDone] = useState(false)
+    const [legDone, setLegDone] = useState(false)
+    const [upperDone, setUpperDone] = useState(false)
+    const [lowerDone, setLowerDone] = useState(false)
+
     return (
         <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/push' element={<Push />} />
-            <Route path='/pull' element={<Pull />} />
-            <Route path='/leg' element={<Leg />} />
-            <Route path='/upper' element={<Upper />} />
-            <Route path='/lower' element={<Lower />} />
+            <Route path='/' element={<Home pushDone={pushDone} pullDone={pullDone} legDone={legDone} upperDone={upperDone} lowerDone={lowerDone}/>} />
+            <Route path='/push' element={<Push setPushDone={setPushDone}/>} />
+            <Route path='/pull' element={<Pull setPullDone={setPullDone}/>} />
+            <Route path='/leg' element={<Leg setLegDone={setLegDone}/>} />
+            <Route path='/upper' element={<Upper setUpperDone={setUpperDone}/>} />
+            <Route path='/lower' element={<Lower setLowerDone={setLowerDone}/>} />
         </Routes>
     )
 }
